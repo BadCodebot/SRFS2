f0544eade3fd94a6819f244aa7da5cde269136f9
trq305 <trq305@gmail.com>
2016-09-24 07:33:32 -0700

    Initial import of IBlockIO interface

---
+++ SRFS.IO/IBlockIO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SRFS.IO {
+
+    /// <summary>
+    /// This interface represents a class that reads and writes data in blocks.
+    /// </summary>
+    /// <remarks>
+    /// All operations are done on a block-by-block bases.  That is, it reads and writes full blocks only and the
+    /// position is set as a number of blocks.  There is no mechanism to add or delete blocks.
+    /// </remarks>
+    public interface IBlockIO : IDisposable {
+
+        /// <summary>
+        /// The number of bytes per block.
+        /// </summary>
+        int BlockSizeBytes { get; }
+
+        /// <summary>
+        /// The number of blocks available.
+        /// </summary>
+        int NBlocks { get; }
+
+        void Read(int position, byte[] buffer, int bufferOffset, int blockCount);
+
+        void Write(int position, byte[] buffer, int bufferOffset, int blockCount);
+    }
+}
