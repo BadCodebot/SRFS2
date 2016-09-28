9ffa297e9a59c6d9e29e0a9d46b3c6a5b2088d06
trq305 <trq305@gmail.com>
2016-09-27 21:10:46 -0700

    Changed class name, updated to use ClusterState enum rather than a byte.

---
+++ SRFS.Model/Clusters/ClusterStatesCluster.cs
@@ -0,0 +1,34 @@
+﻿namespace SRFS.Model.Clusters {
+
+    public class ClusterStatesCluster : ArrayCluster<ClusterState> {
+
+        // Public
+        #region Constructors
+
+        public ClusterStatesCluster() : base(sizeof(ClusterState)) { }
+
+        #endregion
+
+        public static int ElementsPerCluster {
+            get {
+                if (!_elementsPerCluster.HasValue) _elementsPerCluster = CalculateElementCount(sizeof(byte));
+                return _elementsPerCluster.Value;
+            }
+        }
+
+        // Protected
+        #region Methods
+
+        protected override void WriteElement(ClusterState value, ByteBlock byteBlock, int offset) {
+            byteBlock.Set(offset, (byte)value);
+        }
+
+        protected override ClusterState ReadElement(ByteBlock byteBlock, int offset) {
+            return (ClusterState)byteBlock.ToByte(offset);
+        }
+
+        #endregion
+
+        private static int? _elementsPerCluster;
+    }
+}
