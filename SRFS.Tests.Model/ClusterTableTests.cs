70b3503aa1aa8c6223cd8ad12a400e19b48e2e33
trq305 <trq305@gmail.com>
2016-10-02 10:19:42 -0700

    Added new tests.

---
+++ SRFS.Tests.Model/ClusterTableTests.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SRFS.Model;
+using SRFS.Model.Data;
+using SRFS.Model.Clusters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SRFS.IO;
+
+namespace SRFS.Tests.Model {
+
+    [TestClass]
+    public class ClusterTableTests {
+
+        private int verifyProperties(ByteBlock b, ClusterType expectedType, int expectedNextClusterAddress) {
+
+            int offset = 0;
+
+            byte[] marker = b.ToByteArray(offset, Constants.SrfsMarker.Length);
+            Assert.IsTrue(marker.SequenceEqual(Constants.SrfsMarker));
+            offset += marker.Length;
+
+            byte[] version = b.ToByteArray(offset, Constants.CurrentVersion.Length);
+            Assert.IsTrue(version.SequenceEqual(Constants.CurrentVersion));
+            offset += version.Length;
+
+            Guid guid = new Guid(b.ToByteArray(offset, Constants.GuidLength));
+            Assert.AreEqual(guid, Configuration.FileSystemID);
+            offset += Constants.GuidLength;
+
+            byte[] signatureBytes = b.ToByteArray(offset, Signature.Length);
+            offset += Signature.Length;
+
+            byte[] thumbprintBytes = b.ToByteArray(offset, KeyThumbprint.Length);
+            Assert.IsTrue(thumbprintBytes.SequenceEqual(Configuration.CryptoSettings.SigningKeyThumbprint.Bytes));
+            offset += KeyThumbprint.Length;
+
+            byte[] hashBytes = b.ToByteArray(offset, 32);
+            offset += 32;
+
+            ClusterType clusterType = (ClusterType)b.ToByte(offset);
+            Assert.AreEqual(clusterType, expectedType);
+            offset += sizeof(ClusterType);
+
+            int nextClusterAddress = b.ToInt32(offset);
+            Assert.AreEqual(nextClusterAddress, expectedNextClusterAddress);
+            offset += sizeof(int);
+
+            return offset;
+        }
+
+        [TestMethod]
+        public void IntClusterTableTest() {
+            ConfigurationTest.Initialize();
+
+            using (var io = new MemoryIO(30 * 1024 * 1024, 512)) {
+
+                Random r = new Random();
+
+                ClusterTable<int> ct = new ClusterTable<int>(new IntArrayCluster[] {
+                    new IntArrayCluster() { Address = 2, Type = ClusterType.BytesUsedTable },
+                    new IntArrayCluster() { Address = 4, Type = ClusterType.BytesUsedTable }
+                });
+
+                ClusterTable<int> ct2 = new ClusterTable<int>(new IntArrayCluster[] {
+                    new IntArrayCluster() { Address = 2, Type = ClusterType.BytesUsedTable },
+                    new IntArrayCluster() { Address = 4, Type = ClusterType.BytesUsedTable }
+                });
+
+                ct.Flush(io);
+                ct2.Load(io);
+
+                // Check that the cluster is written and everything is zeroed
+
+                ByteBlock b = new ByteBlock(io.Bytes, 2 * Configuration.Geometry.BytesPerCluster, Configuration.Geometry.BytesPerCluster);
+                int offset = verifyProperties( b, ClusterType.BytesUsedTable, 4);
+                for (int i = 0; i < ArrayCluster.CalculateElementCount(sizeof(int)); i++) {
+                    Assert.AreEqual(b.ToInt32(offset + i * sizeof(int)), 0);
+                }
+
+                b = new ByteBlock(io.Bytes, 4 * Configuration.Geometry.BytesPerCluster, Configuration.Geometry.BytesPerCluster);
+                offset = verifyProperties( b, ClusterType.BytesUsedTable, Constants.NoAddress);
+                for (int i = 0; i < ArrayCluster.CalculateElementCount(sizeof(int)); i++) {
+                    Assert.AreEqual(b.ToInt32(offset + i * sizeof(int)), 0);
+                }
+
+                for (int i = 0; i < ct2.Count; i++) {
+                    Assert.AreEqual(ct2[i], 0);
+                }
+
+                // Now randomize the contents
+                for (int i = 0; i < ct.Count; i++) ct[i] = r.Next();
+                ct.Flush(io);
+
+                b = new ByteBlock(io.Bytes, 2 * Configuration.Geometry.BytesPerCluster, Configuration.Geometry.BytesPerCluster);
+                offset = verifyProperties(b, ClusterType.BytesUsedTable, 4);
+                int index = 0;
+                for (int i = 0; i < ArrayCluster.CalculateElementCount(sizeof(int)); i++, index++) {
+                    Assert.AreEqual(b.ToInt32(offset + i * sizeof(int)), ct[index]);
+                }
+
+                b = new ByteBlock(io.Bytes, 4 * Configuration.Geometry.BytesPerCluster, Configuration.Geometry.BytesPerCluster);
+                offset = verifyProperties(b, ClusterType.BytesUsedTable, Constants.NoAddress);
+                for (int i = 0; i < ArrayCluster.CalculateElementCount(sizeof(int)); i++, index++) {
+                    Assert.AreEqual(b.ToInt32(offset + i * sizeof(int)), ct[index]);
+                }
+
+                ct2.Load(io);
+                for (int i = 0; i < ct2.Count; i++) {
+                    Assert.AreEqual(ct2[i], ct[i]);
+                }
+
+                // Add a cluster
+                ct.AddCluster(new IntArrayCluster() { Address = 7, Type = ClusterType.BytesUsedTable });
+                ct2.AddCluster(new IntArrayCluster() { Address = 7, Type = ClusterType.BytesUsedTable });
+                ct.Flush(io);
+
+                // Make sure that next cluster is updated
+                b = new ByteBlock(io.Bytes, 4 * Configuration.Geometry.BytesPerCluster, Configuration.Geometry.BytesPerCluster);
+                verifyProperties(b, ClusterType.BytesUsedTable, 7);
+
+                // check the new cluster and assure everything is zero
+                b = new ByteBlock(io.Bytes, 7 * Configuration.Geometry.BytesPerCluster, Configuration.Geometry.BytesPerCluster);
+                offset = verifyProperties(b, ClusterType.BytesUsedTable, Constants.NoAddress);
+                for (int i = 0; i < ArrayCluster.CalculateElementCount(sizeof(int)); i++) {
+                    Assert.AreEqual(b.ToInt32(offset + i * sizeof(int)), 0);
+                }
+
+                ct2.Load(io);
+                for (int i = 0; i < ct2.Count; i++) {
+                    Assert.AreEqual(ct2[i], ct[i]);
+                }
+
+                Assert.AreEqual(ct2.Count, 3 * ArrayCluster.CalculateElementCount(sizeof(int)));
+
+                // Remove a cluster
+                ct.RemoveLastCluster();
+                ct.Flush(io);
+
+                // Make sure that the last cluster is updated
+                b = new ByteBlock(io.Bytes, 4 * Configuration.Geometry.BytesPerCluster, Configuration.Geometry.BytesPerCluster);
+                verifyProperties(b, ClusterType.BytesUsedTable, Constants.NoAddress);
+            }
+        }
+
+    }
+}
---
+++ SRFS.Tests.Model/Clusters/ObjectArrayClusterTests.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SRFS.Model;
+using System.Security.Cryptography;
+using SRFS.Model.Clusters;
+using SRFS.IO;
+using SRFS.Model.Data;
+using System.Security.Principal;
+using System.Security.AccessControl;
+
+namespace SRFS.Tests.Model.Clusters {
+
+    [TestClass]
+    public class ObjectArrayClusterTests {
+
+        private int verifyProperties(ByteBlock b, int offset, ClusterType type) {
+
+            byte[] marker = b.ToByteArray(offset, Constants.SrfsMarker.Length);
+            Assert.IsTrue(marker.SequenceEqual(Constants.SrfsMarker));
+            offset += marker.Length;
+
+            byte[] version = b.ToByteArray(offset, Constants.CurrentVersion.Length);
+            Assert.IsTrue(version.SequenceEqual(Constants.CurrentVersion));
+            offset += version.Length;
+
+            Guid guid = new Guid(b.ToByteArray(offset, Constants.GuidLength));
+            Assert.AreEqual(guid, Configuration.FileSystemID);
+            offset += Constants.GuidLength;
+
+            byte[] signatureBytes = b.ToByteArray(offset, Signature.Length);
+            offset += Signature.Length;
+
+            byte[] thumbprintBytes = b.ToByteArray(offset, KeyThumbprint.Length);
+            Assert.IsTrue(thumbprintBytes.SequenceEqual(Configuration.CryptoSettings.SigningKeyThumbprint.Bytes));
+            offset += KeyThumbprint.Length;
+
+            byte[] hashBytes = b.ToByteArray(offset, 32);
+            offset += 32;
+
+            ClusterType clusterType = (ClusterType)b.ToByte(offset);
+            Assert.AreEqual(clusterType, type);
+            offset += sizeof(ClusterType);
+
+            int nextClusterAddress = b.ToInt32(offset);
+            Assert.AreEqual(nextClusterAddress, 3);
+            offset += sizeof(int);
+
+            return offset;
+        }
+
+        private static DateTime creationTime = new DateTime(2000, 1, 1);
+        private static DateTime lastWriteTime = new DateTime(2001, 1, 1);
+        private static DateTime lastAccessTime = new DateTime(2002, 1, 1);
+        private static SecurityIdentifier group = new SecurityIdentifier(WellKnownSidType.NullSid, null);
+        private static SecurityIdentifier owner = WindowsIdentity.GetCurrent().Owner;
+        private static int Address = 2;
+        private static int NextClusterAddress = 3;
+        private static int BlockSize = 512;
+        private static int RandomNumberGeneratorSeed = 1298;
+        private static Random _random = new Random(RandomNumberGeneratorSeed);
+
+        private Directory createDirectory(int index) {
+            return new Directory(index, index.ToString()) {
+                Attributes = System.IO.FileAttributes.Normal,
+                CreationTime = creationTime,
+                LastWriteTime = lastWriteTime,
+                LastAccessTime = lastAccessTime,
+                ParentID = index + 1,
+                Group = group,
+                Owner = owner
+            };
+        }
+
+        private File createFileEntry(int index) {
+            return new File(index, index.ToString()) {
+                Attributes = System.IO.FileAttributes.Normal,
+                CreationTime = creationTime,
+                LastWriteTime = lastWriteTime,
+                LastAccessTime = lastAccessTime,
+                ParentID = index + 1,
+                Group = group,
+                Owner = owner,
+                Length = _random.Next(),
+                FirstCluster = _random.Next()
+            };
+        }
+
+        private SrfsAccessRule createAccessRule(int index) {
+            FileSystemAccessRule r = new FileSystemAccessRule(WindowsIdentity.GetCurrent().User, FileSystemRights.FullControl, AccessControlType.Allow);
+            return new SrfsAccessRule(FileSystemObjectType.File, _random.Next(), r);
+        }
+
+        private SrfsAuditRule createAuditRule(int index) {
+            FileSystemAuditRule r = new FileSystemAuditRule(WindowsIdentity.GetCurrent().User, FileSystemRights.FullControl, AuditFlags.Success);
+            return new SrfsAuditRule(FileSystemObjectType.File, _random.Next(), r);
+        }
+
+        private void saveLoadTest<T>(Func<ObjectArrayCluster<T>> clusterFactory, Func<int, T> elementFactory,
+            Func<ByteBlock, int, T> elementReader, int elementLength) where T : class {
+
+            ConfigurationTest.Initialize();
+
+            using (var io = new MemoryIO(30 * Configuration.Geometry.BytesPerCluster, BlockSize)) {
+
+                var csc = clusterFactory();
+
+                for (int i = 0; i < csc.Count; i++) csc[i] = elementFactory(i);
+
+                csc.NextClusterAddress = NextClusterAddress;
+                csc.Save(io);
+
+                int offset = 0;
+                ByteBlock b = new ByteBlock(io.Bytes,
+                    Address * Configuration.Geometry.BytesPerCluster, Configuration.Geometry.BytesPerCluster);
+
+                offset = verifyProperties(b, offset, csc.Type);
+
+                T[] cs = new T[csc.Count];
+                for (int i = 0; i < csc.Count; i++) {
+                    if (b.ToBoolean(offset)) {
+                        offset += sizeof(bool);
+                        cs[i] = elementReader(b, offset);
+                    } else {
+                        offset += sizeof(bool);
+                        cs[i] = null;
+                    }
+                    offset += elementLength;
+                }
+                Assert.IsTrue(cs.SequenceEqual(csc));
+
+                var csc2 = clusterFactory();
+                csc2.Address = Address;
+                csc2.Load(io);
+                Assert.AreEqual(csc.ID, csc2.ID);
+                Assert.AreEqual(csc.Type, csc2.Type);
+                Assert.AreEqual(csc.NextClusterAddress, csc2.NextClusterAddress);
+
+                Assert.IsTrue(csc.SequenceEqual(csc2));
+            }
+        }
+
+        [TestMethod]
+        public void DirectoryEntryClusterSaveLoadTest() {
+
+            saveLoadTest(() => Directory.CreateArrayCluster(Address), 
+                createDirectory, 
+                (b, offset) => new Directory(b, offset), 
+                Directory.StorageLength);
+        }
+
+        [TestMethod]
+        public void FileEntryClusterSaveLoadTest() {
+
+            saveLoadTest(() => File.CreateArrayCluster(Address),
+                createFileEntry,
+                (b, offset) => new File(b, offset),
+                File.StorageLength);
+        }
+
+        [TestMethod]
+        public void AccessRuleClusterSaveLoadTest() {
+
+            saveLoadTest(() => SrfsAccessRule.CreateArrayCluster(Address),
+                createAccessRule,
+                (b, offset) => new SrfsAccessRule(b, offset),
+                SrfsAccessRule.StorageLength);
+        }
+
+        [TestMethod]
+        public void AuditRuleClusterSaveLoadTest() {
+
+            saveLoadTest(() => SrfsAuditRule.CreateArrayCluster(Address),
+                createAuditRule,
+                (b, offset) => new SrfsAuditRule(b, offset),
+                SrfsAuditRule.StorageLength);
+        }
+    }
+}
