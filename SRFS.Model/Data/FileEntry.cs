42df43214a5cc4ca9cc6ac0383109e27e56a8663
trq305 <trq305@gmail.com>
2016-10-01 19:53:37 -0700

    Added extensions for ByteBlock and save, constructor, equals and supporting methods.

--- SRFS.Model/Data/FileEntry.cs
+++ SRFS.Model/Data/FileEntry.cs
@@ -1,8 +1,38 @@
-﻿namespace SRFS.Model.Data {
+﻿using System;
 
-    public class FileEntry : FileSystemEntry {
+namespace SRFS.Model.Data {
 
-        public FileEntry(FileSystem fileSystem, int id, string name) : base(fileSystem, id, name) { }
+    public sealed class FileEntry : FileSystemEntry, IEquatable<FileEntry> {
+
+        public FileEntry(int id, string name) : base(id, name) {
+            _length = 0;
+            _firstCluster = Constants.NoAddress;
+        }
+
+        public FileEntry(ByteBlock byteBlock, int offset) : base(byteBlock, offset) {
+            offset += FileSystemEntryStorageLength;
+
+            _length = byteBlock.ToInt64(offset);
+            offset += sizeof(long);
+
+            _firstCluster = byteBlock.ToInt32(offset);
+        }
+
+        public bool Equals(FileEntry other) {
+            return base.Equals(other) &&
+                _length == other._length &&
+                _firstCluster == other._firstCluster;
+        }
+
+        public override void Save(ByteBlock byteBlock, int offset) {
+            base.Save(byteBlock, offset);
+            offset += FileSystemEntryStorageLength;
+
+            byteBlock.Set(offset, _length);
+            offset += sizeof(long);
+
+            byteBlock.Set(offset, _firstCluster);
+        }
 
         public long Length {
             get {
@@ -26,5 +56,17 @@
 
         private long _length;
         private int _firstCluster;
+    }
+
+    public static class FileEntryExtensions {
+
+        public static void Set(this ByteBlock byteBlock, int offset, FileEntry entry) {
+            if (entry == null) throw new ArgumentNullException(nameof(entry));
+            entry.Save(byteBlock, offset);
+        }
+
+        public static FileEntry ToFileEntry(this ByteBlock byteBlock, int offset) {
+            return new FileEntry(byteBlock, offset);
+        }
     }
 }
