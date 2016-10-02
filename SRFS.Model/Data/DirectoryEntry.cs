e997f2c014554a55ee64fc27bb4932aa579ee61f
trq305 <trq305@gmail.com>
2016-10-01 19:53:25 -0700

    Added extensions for ByteBlock and save, constructor, equals and supporting code. Changed Attributes to ensure that Directory is always set.

--- SRFS.Model/Data/DirectoryEntry.cs
+++ SRFS.Model/Data/DirectoryEntry.cs
@@ -1,14 +1,45 @@
 ﻿using System.Collections.Generic;
 using System.Security.AccessControl;
+using System.IO;
+using System;
+using System.Diagnostics;
+using System.Security.Principal;
 
 namespace SRFS.Model.Data {
 
-    public class DirectoryEntry : FileSystemEntry {
+    public sealed class DirectoryEntry : FileSystemEntry, IEquatable<DirectoryEntry> {
 
-        public DirectoryEntry(FileSystem fileSystem, int id, string name) : base(fileSystem, id, name) { }
+        public DirectoryEntry(int id, string name) : base(id, name) {
+            Attributes = FileAttributes.Normal;
+        }
 
-        public IDictionary<string,DirectoryEntry> SubDirectories => FileSystem.GetContainedDirectories(this);
+        public DirectoryEntry(ByteBlock byteBlock, int offset) : base(byteBlock, offset) { }
 
-        public IDictionary<string, FileEntry> Files => FileSystem.GetContainedFiles(this);
+        public override FileAttributes Attributes {
+            get {
+                return base.Attributes | FileAttributes.Directory;
+            }
+            set {
+                base.Attributes = value | FileAttributes.Directory;
+            }
+        }
+
+        public bool Equals(DirectoryEntry other) {
+            return base.Equals(other);
+        }
+
+        public const int StorageLength = FileSystemEntryStorageLength;
+    }
+
+    public static class DirectoryEntryExtensions {
+
+        public static void Set(this ByteBlock byteBlock, int offset, DirectoryEntry entry) {
+            if (entry == null) throw new ArgumentNullException(nameof(entry));
+            entry.Save(byteBlock, offset);
+        }
+
+        public static DirectoryEntry ToDirectoryEntry(this ByteBlock byteBlock, int offset) {
+            return new DirectoryEntry(byteBlock, offset);
+        }
     }
 }
