643570a993944780a9be68e14b22f7af0cf83058
trq305 <trq305@gmail.com>
2016-10-08 09:28:25 -0700

    Changed configuration to create new random GUID.

--- SRFS.Tests.Model/ConfigurationTest.cs
+++ SRFS.Tests.Model/ConfigurationTest.cs
@@ -5,15 +5,32 @@ using System.Text;
 using System.Threading.Tasks;
 using SRFS.Model;
 using System.Security.Cryptography;
+using SRFS.IO;
 
 namespace SRFS.Tests.Model {
     public class ConfigurationTest {
 
+        private static int BlockSize = 512;
+        private static int BytesPerCluster = 16 * 1024;
+        private static int ClustersPerTrack = 256;
+        private static int DataClustersPerTrack = 256 - 32;
+        private static int TrackCount = 128;
+
+        public static MemoryIO CreateMemoryIO() {
+            return new MemoryIO(RequiredSize, BlockSize);
+        }
+
+        public static int RequiredSize {
+            get {
+                return BytesPerCluster * ClustersPerTrack * TrackCount + 2 * BlockSize;
+            }
+        }
+
         public static void Initialize() {
             if (_isInitialized) return;
 
-            Configuration.Geometry = new Geometry(1024 * 1024, 10, 9, 2);
-            Configuration.FileSystemID = new Guid();
+            Configuration.Geometry = new Geometry(BytesPerCluster, ClustersPerTrack, DataClustersPerTrack, TrackCount);
+            Configuration.FileSystemID = Guid.NewGuid();
             Configuration.VolumeName = "Memory Test Volume";
             Configuration.Options = Options.None;
             CngKey pk = CngKey.Create(CngAlgorithm.ECDiffieHellmanP521, null, new CngKeyCreationParameters { ExportPolicy = CngExportPolicies.AllowPlaintextExport });
