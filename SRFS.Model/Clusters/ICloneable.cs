e17f49d190b41a788bb95cccdf515011b6e7c2bf
trq305 <trq305@gmail.com>
2016-10-15 10:55:01 -0700

    Generic ICloneable

---
+++ SRFS.Model/Clusters/ICloneable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRFS.Model.Clusters {
+    public interface ICloneable<out T> {
+        T Clone();
+    }
+}
